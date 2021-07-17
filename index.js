9a349411a7c385ba01cf0401257a203d25c6b304
Hannah <hannahwoodward@hotmail.com>
2021-07-17 15:46:10 +0800

    Tested correctly in replit

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    console.log('Hi, my name is ' + name);
+  }
+  introduction('Josh');
+  introduction('Nancy');
+  
+  function introductionWithLanguage(name, language) {
+    console.log('Hi, my name is ' + name + ' and I am learning to program in ' + language + '.');
+  }
+  introductionWithLanguage('Josh', 'Ember.js');
+  introductionWithLanguage('Nancy', 'React');
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`Hi, my name is, ${name}, and I am learning to program in ${language}.`);
+  }
+  
+  introductionWithLanguageOptional("Gracie");
+  introductionWithLanguageOptional("Gracie", "Python");
